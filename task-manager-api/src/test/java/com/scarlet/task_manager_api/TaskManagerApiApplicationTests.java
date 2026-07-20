3c3c2534fba82e4d11767b6006ebfab4a2d2335e
ScarletRuelas <Zarcsextans@gmail.com>
2026-07-20 10:55:04 -0600

    Initialize Spring Boot project structure

---
+++ task-manager-api/src/test/java/com/scarlet/task_manager_api/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlet.task_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
