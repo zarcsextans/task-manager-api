320ab7795ee8fa4fcfa829724d21955ffa661aa7
ScarletRuelas <Zarcsextans@gmail.com>
2026-07-21 10:20:44 -0600

    feat: Create task entities

--- task-manager-api/src/main/java/com/scarlet/task_manager_api/entity/Project.java
+++ task-manager-api/src/main/java/com/scarlet/task_manager_api/entity/Project.java
@@ -1,4 +1,30 @@
 package com.scarlet.task_manager_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(
+            mappedBy = "project",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Task> tasks;
 }
--- task-manager-api/src/main/java/com/scarlet/task_manager_api/entity/Task.java
+++ task-manager-api/src/main/java/com/scarlet/task_manager_api/entity/Task.java
@@ -1,4 +1,25 @@
 package com.scarlet.task_manager_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
